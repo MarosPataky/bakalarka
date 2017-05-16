f206bd0862545986e29814b37d9a90443a059a76
Maros Pataky <pataky.maros@gmail.com>
2017-05-16 15:05:58 +0200

    Added config from consul to TurbineHystrix project

--- TurbineHystrixDashboard/src/main/java/sk/pataky/HystrixDashboardApplication.java
+++ TurbineHystrixDashboard/src/main/java/sk/pataky/HystrixDashboardApplication.java
@@ -1,6 +1,7 @@
 package sk.pataky;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,10 +13,18 @@ import org.springframework.core.env.Environment;
 @EnableHystrixDashboard
 @EnableTurbine
 @EnableDiscoveryClient
-public class HystrixDashboardApplication {
+public class HystrixDashboardApplication implements CommandLineRunner {
+
+	@Autowired
+	Environment environment;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Testing property value = " + environment.getProperty("testing.property"));
 	}
 }
